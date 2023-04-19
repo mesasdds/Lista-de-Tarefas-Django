bd42118d886630603d7773d04c4bc5b1069e2b9b
mesasdds <mesasdds@gmail.com>
2023-04-19 11:38:23 -0300

    confirmação de delete das tasks

--- tasks/static/js/scripts.js
+++ tasks/static/js/scripts.js
@@ -1,1 +1,13 @@
-console.log("Funciona!");
+$(document).ready(function){
+    var deleteBtn = $('.delete-btnc')
+    $(deleteBtn).on('click', function(e){
+        e.preventDefault()
+
+        var delLink = $(this).attr('href')
+        var result = confirm('Quer deletar esta tarefa?')
+
+        if(result){
+            window.location.href = delLink
+        }
+    })
+}
