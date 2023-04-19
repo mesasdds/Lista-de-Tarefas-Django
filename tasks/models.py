6cb0c86f78844ec3c8b5a20e01dd1af12f88bbcb
mesasdds <mesasdds@gmail.com>
2023-04-19 11:10:52 -0300

    Adição da interação com o banco de dados, para exclusao/adição de tarefas e o painel de admn já contentado a taks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+STATUS = (
+    ('doing', 'Doing'),
+    ('done', 'Done')
+)
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    done = models.CharField(max_length=5, choices=STATUS, )
+    create_at = models.DateTimeField(auto_now_add=True) #criação do campo data para o Banco
+    update_at = models.DateTimeField(auto_now=True) #praticamente um commit confirmando a criação em banco da data
+    def __str__(self):
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .models import Task
+from .forms import TaskForm
+from django.contrib import messages
+
 
 # Create your views here.
+def taskList(request):
+    tasks = Task.objects.all().order_by('-create_at')
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})
+
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            task = form.save(commit = False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/addtask.html', {'form': form})
+
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+    if(request.method == 'POST'):
+        form = TaskForm(request.POST, instance=task)
+        if(form.is_valid()):
+            task.save()
+            return redirect('/')
+        else:
+            return render(request, 'tasks/edittask.html', {'form':form, 'task':task})
+    else:
+        return render(request, 'tasks/edittask.html', {'form':form, 'task':task})
+
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+
+    messages.info(request, 'Tarefa Deletada com Sucesso!')
+    return redirect('/')
+
+
 def helloWorld(request):
     return HttpResponse('Hello World!')
 
-def taskList(request):
-    return render(request, 'tasks/list.html')
-
 def yourName(request, name):
     return render(request, 'tasks/yourname.html', {'nome':name}) #sera util futuramente, por enqunato é meio bobo
+
 
